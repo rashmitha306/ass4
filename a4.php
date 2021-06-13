da0ca115d241103c6814e1da8a9ff1cbde1d9bed
rashmitha306 <rashmitha306>
2021-06-13 14:33:23 +0530

    first commit

---
+++ a4.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<p>Number generated randomly is  <?php
+    $rand = rand(1, 100);
+    echo $rand;
+?>.And its square root is <?php
+    echo sqrt($rand);
+?>.</p>
+</body>
+</html>
