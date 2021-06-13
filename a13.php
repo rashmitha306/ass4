265d42b99f3d6722d46ed2632d24bbb4b1680b84
rashmitha306 <rashmitha306>
2021-06-13 15:59:10 +0530

    added a13

---
+++ a13.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<?php
+function factorial_of_a_number($n)
+{
+  if($n ==0)
+    {
+	   return 1;
+    }
+  else 
+    {	
+	   return $n * factorial_of_a_number($n-1);
+    }
+	}
+print_r(factorial_of_a_number(4)."\n");
+?>
+
+
+</body>
+</html>
